2416358a4078e2f646a217b71f3ef2a77107af93
huzai0101 <152717803+huzai0101@users.noreply.github.com>
2023-12-04 00:01:49 +0800

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+Thanks for downloading this template!
+
+Name: Aisar
+Author: BootstrapMade.com
+License: https://bootstrapmade.com/license/
---
+++ script.js
@@ -0,0 +1,12 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
+ */
+
+var typed = new Typed(".multiple-text", {
+    strings: ["Fronted Developer", "Youtuber", "Blogger"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
